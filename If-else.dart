15fd3196daa1a4d44d354198624a34dee8bfb1c4
mdshaheimran <mdshaheimran54@gmail.com>
2024-02-24 14:36:09 +0600

    if-else

---
+++ If-else.dart
@@ -0,0 +1,14 @@
+main (){
+  //Jodi chuti pai thole ami vari jabo,
+  bool gotLeave = false;
+  if (gotLeave == true){
+  print('Varite jabo ');}
+
+  //nahole office korbo
+
+  else {
+    print("Office korbo");
+  }
+
+
+}
