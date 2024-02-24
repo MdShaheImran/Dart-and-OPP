d86368727bac68f31945cf510b11db228cf7c2ab
mdshaheimran <mdshaheimran54@gmail.com>
2024-02-24 14:48:43 +0600

    if-else

--- If-else.dart
+++ If-else.dart
@@ -10,5 +10,16 @@ main (){
     print("Office korbo");
   }
 
+  //jodi amr kashe 5000 taka thake ami edi e shobar jonno shopping kore neya jabo
+  int takaaca = 5000;
+  if(takaaca < 5001){
+    print("Shopping Korbo sobar jonno");
+
+  }
+  else {
+    print("taka manage kore tarpor Korbo In Sha Allah!");
+  }
+
+
 
 }
