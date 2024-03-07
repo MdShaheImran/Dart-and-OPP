46ab8c123e6d746a6c122a7242abb293f3b196fa
MdShaheImran <mdshaheimran54@gmail.com>
2024-03-07 15:22:04 +0600

    Update Functions

---
+++ Functions.dart
@@ -0,0 +1,19 @@
+main() {
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+  welcomeMessage();
+}
+  //Functrions
+
+  welcomeMessage(){
+    //Body
+    print("WelCome to my functions practice");
+    print("Hello world");
+  }
