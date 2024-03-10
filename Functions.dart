edaaa6f9e037817d13746ffa5ac628b846ee0821
MdShaheImran <mdshaheimran54@gmail.com>
2024-03-10 11:21:05 +0600

    Update Functions

--- Functions.dart
+++ Functions.dart
@@ -1,20 +1,21 @@
 main() {
 
   //function call
-  welcomeMessage("Imran");
-  welcomeMessage("Noyon");
-  welcomeMessage("Maruf");
-  welcomeMessage("Riyad");
-  welcomeMessage("Zalish");
-  welcomeMessage("Ali");
-  welcomeMessage("Billal");
-  welcomeMessage("Kamal");
-  welcomeMessage("Tanvir");
-  welcomeMessage("Rahat");
+  welcomeMessage("Imran", "Chittagong", 20);
+  welcomeMessage("Noyon", "Dinazpur", 26);
+  welcomeMessage("Maruf", "Mymenshingh", 26);
+  welcomeMessage("Riyad", "Cumilla", 20);
+  welcomeMessage("Zalish", "Dinazpur" ,26);
+  welcomeMessage("Ali" ,"Nayranganj", 26);
+  welcomeMessage("Billal", 'Dhaka' ,23);
+  welcomeMessage("Kamal" ,"Rangpur", 23);
+  welcomeMessage("Tanvir", "Gazipur", 20);
+  welcomeMessage("Rahat", 'Borishal',28);
 }
+
   //Functrions
 
-  welcomeMessage(String name){
+  welcomeMessage(String name, String address, int age) {
     //Body
     print("WelCome to my functions practice");
     print("What are you doing now $name?");
